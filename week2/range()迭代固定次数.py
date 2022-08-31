14a9fc893d2d8bc114aa7ed0e7978fcdf9c2133a
lishubin <2697601945@qq.com>
2022-08-31 23:36:21 +0800

    更新模块1.1.0

--- week2/range()迭代固定次数.py
+++ week2/range()迭代固定次数.py
@@ -1,13 +1,11 @@
-# @Time : 2022/8/31  16:54
+# @Time: 2022/8/31 22:59
 # @Author: 李树斌
-# @File : nester1.py
-# @Software : PyCharm
-def print_lol(the_list):
-    for each_item in the_list:
-        if isinstance(each_item,list):
-            print_lol(each_item)
-        else:
-            print(each_item)
+# @File : range()迭代固定次数.py
+# @Software :PyCharm
+movies = ["The Holy grail", 1975, "Terry Jones & Terry Gilliam", 91,
+          ["Graham Chapman",
+            ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+movies1 = ["The Holy grail", 1975, "Terry Jones & Terry Gilliam", 91,]
 def print_lol(the_list,level):
     """这个函数有一个位置参数叫"the_list",
     这可以是任何python列表（包含或者不包含嵌套列表），
@@ -20,3 +18,6 @@ def print_lol(the_list,level):
             for tab_stop in range(level):
                 print("\t",end='')
             print(each_item)
+#进行输出的对比
+print_lol(movies,0)
+print_lol(movies1,0)
