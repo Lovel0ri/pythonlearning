339429935fe94531dee31e5971028492f8b79990
lishubin <2697601945@qq.com>
2022-09-01 00:06:20 +0800

    修复测试nester0模块.py函数调用参数不足

--- week2/测试nester0模块.py
+++ week2/测试nester0模块.py
@@ -3,9 +3,9 @@
 # @File : 测试nester0模块.py
 # @Software : PyCharm
 
-#暂时有bug没修复，无法调用nester1
+#暂时有bug没修复，无法调用nester1(已修复）
 import nester1
 movies = ["The Holy grail", 1975, "Terry Jones & Terry Gilliam", 91,
           ["Graham Chapman",
             ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
-nester1.print_lol(movies)
+nester1.print_lol(movies,0)
