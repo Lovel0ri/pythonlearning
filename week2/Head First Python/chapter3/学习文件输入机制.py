10b01a0c0e55fd5b2dc2bda3c60ed70624ed2286
lishubin <2697601945@qq.com>
2022-09-01 11:59:37 +0800

    添加split函数

--- week2/Head First Python/chapter3/学习文件输入机制.py
+++ week2/Head First Python/chapter3/学习文件输入机制.py
@@ -7,3 +7,15 @@ import os
 os.chdir('../chapter3')
 #查看工作目录是否正确
 print(os.getcwd())
+
+#打开文件并赋值到data,输出单个数据行
+data = open('sketch.txt')
+# print(data.readline(),end='')
+# print(data.readline(),end='')
+
+data.seek(0)
+for each_line in data:
+    (role,line_spoken) = each_line.split(':')#用split赋值到role和line_spoken
+    print(role,end='')#end=' '意思是末尾不换行，加空格
+    print(" said:",end='')
+    print(line_spoken,end='')
