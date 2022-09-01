2a78ae8fd5ded0bf8366b5712b43846cab6553c4
lishubin <2697601945@qq.com>
2022-09-01 13:41:38 +0800

    增加find函数修复输出问题

---
+++ week2/Head First Python/chapter3/sketch.py
@@ -0,0 +1,23 @@
+# @Time: 2022/9/1 12:06
+# @Author: 李树斌
+# @File : sketch.py
+# @Software :PyCharm
+import os
+os.chdir('../chapter3')
+#查看工作目录是否正确
+print(os.getcwd())
+
+#打开文件并赋值到data,输出单个数据行
+data = open('sketch.txt')
+# print(data.readline(),end='')
+# print(data.readline(),end='')
+
+data.seek(0)
+for each_line in data:
+    if not each_line.find(':') == -1:
+        (role, line_spoken) = each_line.split(':', 1)  # 用split赋值到role和line_spoken
+        print(role, end='')  # end=' '意思是末尾不换行，加空格
+        print(" said:", end='')
+        print(line_spoken, end='')
+
+data.close()
