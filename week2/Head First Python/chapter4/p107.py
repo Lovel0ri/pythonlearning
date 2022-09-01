e83a62c27e05388e0e59018007357fd22a598261
lishubin <2697601945@qq.com>
2022-09-01 18:36:14 +0800

    用finally减少数据破坏错误的可能性

--- week2/Head First Python/chapter4/p107.py
+++ week2/Head First Python/chapter4/p107.py
@@ -38,7 +38,14 @@ try:
     #把台词分别输出到两个data文件
     print(man,file=data_man)
     print(other,file=data_other)
-    data_man.close()
-    data_other.close()
+
+    # data_man.close()#如果上面两个输出有一个出错，这两个文件就无法关闭，可能导致数据丢失被破坏
+    # data_other.close()
 except IOError:
     print('File error')
+
+#如果没有出现任何运行时的错误，会执行finall组中的代码。如果出现IOerror，会执行except组，然后运行finally组。
+#无论如何都要关闭文件
+finally:
+    data_man.close()
+    data_other.close()
