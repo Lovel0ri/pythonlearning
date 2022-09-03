8305e851fb24370d0560f7ceaa8f886dabb53bab
lishubin <2697601945@qq.com>
2022-09-03 22:21:30 +0800

    使用set工厂函数去除重复项

--- week2/Head First Python/chapter5/p140+p145+p152.py
+++ week2/Head First Python/chapter5/p140+p145+p152.py
@@ -2,9 +2,9 @@
 # @Author: 李树斌
 # @File : p140+p145+p152.py
 # @Software :PyCharm
-from sanitizeb import sanitizeb
+import sanitizeb
 
-import nesterbing
+
 # def sanitize(time_string):
 #     if '-' in time_string:
 #         splitter = '-'
@@ -17,25 +17,33 @@ import nesterbing
 
 with open("hfpy_ch5_data/james.txt") as jaf:
     data = jaf.readline()
-    james=data.strip().split(',')#split() 通过指定分隔符对字符串进行切片
+    # split() 通过指定分隔符对字符串进行切片
+    james = data.strip().split(',')
 with open("hfpy_ch5_data/julie.txt") as juf:
     data = juf.readline()
-    julie=data.strip().split(',')
+    julie = data.strip().split(',')
 with open("hfpy_ch5_data/mikey.txt") as mif:
     data = mif.readline()
     mikey = data.strip().split(',')
 with open("hfpy_ch5_data/sarah.txt") as saf:
     data = saf.readline()
     sarah = data.strip().split(',')
-# jamesp =sorted(james)
-# juliep =sorted(julie)
-# mikeyp =sorted(mikey)
-# sarahp =sorted(sarah)
 
-clean_james =sorted([sanitizeb(t) for t in james])
-clean_julie = sorted([sanitizeb(t) for t in julie])
-clean_mikey = sorted([sanitizeb(t) for t in mikey])
-clean_sarah = sorted([sanitizeb(t) for t in sarah])
+print(sorted(set([sanitizeb.sanitizeb(t) for t in james]))[0:3])
+print(sorted(set([sanitizeb.sanitizeb(t) for t in julie]))[0:3])
+print(sorted(set([sanitizeb.sanitizeb(t) for t in mikey]))[0:3])
+print(sorted(set([sanitizeb.sanitizeb(t) for t in sarah]))[0:3])
+
+# unique_james = []
+#
+# for each_t in clean_james:
+#     if each_t not in clean_james:
+#         unique_james.append(each_t)
+#
+# #无法输出数据
+# print(unique_james)
+#
+# #
 
 # for each_t in james:
 #     clean_james.append(sanitizeb.sanitizeb(each_t))
@@ -45,7 +53,3 @@ clean_sarah = sorted([sanitizeb(t) for t in sarah])
 #     clean_mikey.append(sanitizeb.sanitizeb(each_t))
 # for each_t in sarah:
 #     clean_sarah.append(sanitizeb.sanitizeb(each_t))
-print(clean_james)
-print(clean_julie)
-print(clean_mikey)
-print(clean_sarah)
---
+++ week2/Head First Python/chapter5/用集合删除重复项.py
@@ -0,0 +1,7 @@
+# @Time: 2022/9/3 22:04
+# @Author: 李树斌
+# @File : 用集合删除重复项.py
+# @Software :PyCharm
+
+
+distances = set()
