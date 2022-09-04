8d8ab67ab4f4f07b0e173b48028c54ab42cae66e
lishubin <2697601945@qq.com>
2022-09-04 15:27:09 +0800

    完成p210代码重写和数据输出

--- week2/Head First Python/chapter6/p175字典处理数据.py
+++ week2/Head First Python/chapter6/p175字典处理数据.py
@@ -2,18 +2,21 @@
 # @Author: 李树斌
 # @File : p175字典处理数据.py
 # @Software :PyCharm
-class Athlete:
-    def __init__(self, a_name, a_dob=None, a_times=[]):
-        self.name = a_name
-        self.dob = a_dob
-        self.times = a_times
-    def top3 (self):
-        return(sorted(set([sanitizeb(t) for t in self.times]))[0:3])
+# class Athlete:
+#     def __init__(self, a_name, a_dob=None, a_times=[]):
+#         self.name = a_name
+#         self.dob = a_dob
+#         self.times = a_times
+#     def top3 (self):
+#         return(sorted(set([sanitizeb(t) for t in self.times]))[0:3])
+#
+#     def add_time(self,time_value):
+#         self.times.append(time_value)
+#     def add_times(self,list_of_times):
+#         self.times.extend(list_of_times)
+#
 
-    def add_time(self,time_value):
-        self.times.append(time_value)
-    def add_times(self,list_of_times):
-        self.times.extend(list_of_times)
+
 
 def sanitizeb(time_string):
     if '-' in time_string:
@@ -31,20 +34,34 @@ def get_coach_data(filename):
             data = f.readline()
             temple = data.strip().split(',')
             return (
-                Athlete(temple.pop(0),temple.pop(0),temple)
+                AthleteList(temple.pop(0),temple.pop(0),temple)
             )
     except IOError as ioerr:
         print("File error" + str(ioerr))
         return (None)
 
+class AthleteList(list):
+    def __init__(self, a_name, a_dob= None, a_times=None):
+        list.__init__([])
+        if a_times is None:
+            a_times = []
+        self.name = a_name
+        self.dob = a_dob
+        self.extend(a_times)
+
+    def top3(self):
+        return(sorted(set([sanitizeb(t) for t in self]))[0:3])
 
 sarah = get_coach_data("hfpy_ch6_data/sarah2.txt")
 james = get_coach_data("hfpy_ch6_data/james2.txt")
-vera = Athlete('Vera Vi')
-vera.add_time('1.31')
-print(vera.top3())
-vera.add_times(['2.22','1-21','2:22'])
-print(vera.top3())
+mikey = get_coach_data('hfpy_ch6_data/mikey2.txt')
+julie = get_coach_data('hfpy_ch6_data/julie2.txt')
+
+# vera = AthleteList('Vera Vi')
+# vera.append('1.31')
+# print(vera.top3())
+# vera.extend(['2.22','1-21','2:22'])
+# print(vera.top3())
 # (sarah_name,sarah_dob) = sarah.pop(0),sarah.pop(0)
 # #唯一可下标的内置函数：
 # # string:  "foobar"[3] == "b"
@@ -59,3 +76,5 @@ print(vera.top3())
 # sarah_data['Time'] = sarah
 print(sarah.name + "'s fastest times are :" + str(sarah.top3()))
 print(james.name + "'s fastest times are :" + str(james.top3()))
+print(mikey.name + "'s fastest times are :" + str(mikey.top3()))
+print(julie.name + "'s fastest times are :" + str(julie.top3()))
