7c711ac07927c674664a4bd3c267b8a4a50c2250
lishubin <2697601945@qq.com>
2022-09-04 16:15:15 +0800

    学习添加键值对

---
+++ week2/PBItoP/chapter 6 dict/6.2.2添加键值对.py
@@ -0,0 +1,10 @@
+# @Time: 2022/9/4 16:09
+# @Author: 李树斌
+# @File : 6.2.2添加键值对.py
+# @Software :PyCharm
+alien_0 = {'color':'green', 'points':5}
+print(alien_0)
+
+alien_0['x_position'] = 0
+alien_0['y_position'] = 25
+print(alien_0)
