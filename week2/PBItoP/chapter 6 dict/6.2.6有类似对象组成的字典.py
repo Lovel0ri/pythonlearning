56a149647eacc7289f8df40efc83b227b39753de
lishubin <2697601945@qq.com>
2022-09-04 17:10:31 +0800

    由类似对象组成的字典

---
+++ week2/PBItoP/chapter 6 dict/6.2.6有类似对象组成的字典.py
@@ -0,0 +1,12 @@
+# @Time: 2022/9/4 17:02
+# @Author: 李树斌
+# @File : 6.2.6有类似对象组成的字典.py
+# @Software :PyCharm
+favourite_languages = {
+    'jen' : 'python',
+    'sarah' : 'C',
+    'edward' : 'ruby',
+    'phil' : 'python'
+}
+language = favourite_languages['sarah'].title()
+print(f"Sarah's favorite language is {language}")
