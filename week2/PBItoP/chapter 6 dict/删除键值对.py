b2488f8b18a61e4beaf278011ddc0a7ad5438c7d
lishubin <2697601945@qq.com>
2022-09-04 17:02:39 +0800

    删除键值对

---
+++ week2/PBItoP/chapter 6 dict/删除键值对.py
@@ -0,0 +1,9 @@
+# @Time: 2022/9/4 16:43
+# @Author: 李树斌
+# @File : 删除键值对.py
+# @Software :PyCharm
+alien_0 = {'color':'green', 'points':5}
+print(alien_0)
+#删除分数
+del alien_0['points']
+print(alien_0)
