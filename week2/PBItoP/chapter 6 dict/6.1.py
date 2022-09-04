475e52ba958329e0ff6583a03385aab0aa5fa9e2
lishubin <2697601945@qq.com>
2022-09-04 16:08:52 +0800

    创建一个简单字典

---
+++ week2/PBItoP/chapter 6 dict/6.1.py
@@ -0,0 +1,10 @@
+# @Time: 2022/9/4 15:58
+# @Author: 李树斌
+# @File : 6.1.py
+# @Software :PyCharm
+
+alien_0 = {'color':'green', 'points':5}
+print(alien_0['color'])
+print(alien_0['points'])
+new_points = alien_0['points']
+print(f"You just earned {new_points} points")
