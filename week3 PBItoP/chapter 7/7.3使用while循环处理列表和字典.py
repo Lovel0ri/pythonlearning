e2be441e58133eb2508bc82edfe53460e056f829
lishubin <2697601945@qq.com>
2022-09-06 22:34:57 +0800

    在列表之间移动元素

---
+++ week3 PBItoP/chapter 7/7.3使用while循环处理列表和字典.py
@@ -0,0 +1,17 @@
+# @Time: 2022/9/6 22:25
+# @Author: 李树斌
+# @File : 7.3使用while循环处理列表和字典.py
+# @Software :PyCharm
+
+unconfirmed_users = ['A','B','C']
+confirmed_users = []
+#验证每个用户，知道没有未验证的用户
+while unconfirmed_users:
+    current_user = unconfirmed_users.pop()
+    print(f"Verifying user:{current_user}")
+    confirmed_users.append(current_user)
+
+    #显示已经验证的用户
+    print(f"\n The following user have been confirmed:")
+    for confirmed_user in confirmed_users:
+        print(confirmed_user.title())
