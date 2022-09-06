7dcfcd8954fc3421941855e16adec9ef12973465
lishubin <2697601945@qq.com>
2022-09-06 18:11:30 +0800

    p52书本练习

---
+++ PBItoP/chapter4/p52练习.py
@@ -0,0 +1,37 @@
+# @Time: 2022/9/6 17:43
+# @Author: 李树斌
+# @File : p52练习.py
+# @Software :PyCharm
+
+#4-3
+# for value in range(1,21):
+#     print(value)
+
+#4-4
+# numbers = [value for value in range(1,100001)]
+# print(numbers)
+
+#4-5
+# numbers = [value for value in range(1,100001)]
+# print(min(numbers))
+# print(max(numbers))
+# print(sum(numbers))
+
+#4-6
+# odd_number = []
+# for value in range(1,21):
+#     if value % 2== 0:
+#         continue
+#     else:
+#         odd_number.append(value)
+# print(odd_number)
+
+#4-7
+# numbers = []
+# for value in range(1,32):
+#     if value % 3 !=0:
+#         continue
+#     else:
+#         numbers.append(value)
+#         for number in numbers:
+#             print(number)
