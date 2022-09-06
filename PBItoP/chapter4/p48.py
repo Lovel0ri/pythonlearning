c9c8af395ef6fcdc9ea124328afeead42436b699
lishubin <2697601945@qq.com>
2022-09-06 17:25:56 +0800

    p48练习

---
+++ PBItoP/chapter4/p48.py
@@ -0,0 +1,10 @@
+# @Time: 2022/9/6 17:21
+# @Author: 李树斌
+# @File : p48.py
+# @Software :PyCharm
+
+#4-1
+pizzas = ['黑松露','夏威夷水果','新奥尔良']
+for pizza in pizzas:
+    print(f"我喜欢{pizza}披萨")
+print("我真的好喜欢吃披萨啊")
