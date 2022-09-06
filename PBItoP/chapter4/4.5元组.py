df2afda3cec56ed3b9c49bd7d6dbe6a518757bae
lishubin <2697601945@qq.com>
2022-09-06 18:47:14 +0800

    p57练习

---
+++ PBItoP/chapter4/4.5元组.py
@@ -0,0 +1,5 @@
+# @Time: 2022/9/6 18:47
+# @Author: 李树斌
+# @File : 4.5元组.py
+# @Software :PyCharm
+
---
+++ PBItoP/chapter4/p57练习.py
@@ -0,0 +1,9 @@
+# @Time: 2022/9/6 18:38
+# @Author: 李树斌
+# @File : p57练习.py
+# @Software :PyCharm
+
+#4-10
+letters = ['A','B','C','D','E']
+print(letters[:3])
+print(letters[1:4])
