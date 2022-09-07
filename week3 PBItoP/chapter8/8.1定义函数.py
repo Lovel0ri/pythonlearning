33c2bd3716128af5e975451fb8ce144a7f615461
lishubin <2697601945@qq.com>
2022-09-07 23:33:21 +0800

    p117练习

---
+++ week3 PBItoP/chapter8/8.1定义函数.py
@@ -0,0 +1,11 @@
+# @Time: 2022/9/7 22:55
+# @Author: 李树斌
+# @File : 8.1定义函数.py
+# @Software :PyCharm
+
+
+def greet_user(username):
+    #文档字符串
+    print(f"Hello!,{username.title()}")
+
+greet_user("bingbing")
---
+++ week3 PBItoP/chapter8/p117.py
@@ -0,0 +1,15 @@
+# @Time: 2022/9/7 23:28
+# @Author: 李树斌
+# @File : p117.py
+# @Software :PyCharm
+
+#8-1
+def display_meesage(message):
+    print(f"在这一章我们学了{message}")
+
+display_meesage("传递实参")
+
+#8-2
+def favorite_book(bookname):
+    print(f"我最喜欢的图书是{bookname}")
+favorite_book("pythond从入门到实践")
