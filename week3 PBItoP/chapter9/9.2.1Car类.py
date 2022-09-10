c8cd5c8c09905b255c34bade3cfb70315c81722e
lishubin <2697601945@qq.com>
2022-09-10 20:06:58 +0800

    9.2.1Car类给属性指定默认值

--- week3 PBItoP/chapter9/9.2.1Car类.py
+++ week3 PBItoP/chapter9/9.2.1Car类.py
@@ -20,6 +20,18 @@ class Car:
         """打印一条指出汽车历程的信息"""
         print(f"这车子已经行驶了{self.odometer_reading}公里")
 
+    def update_odometer(self,km):
+        """将里程表设为指定的值
+        禁止回调里程表"""
+        if km >= self.odometer_reading:
+            self.odometer_reading = km
+        else:
+            print("不可以把里程表往回调！")
+
 my_new_car = Car("audi","a4",2019)
 print((my_new_car.get_descriptive_name()))
+my_new_car.read_odometer()
+my_new_car.update_odometer(20)
+my_new_car.read_odometer()
+my_new_car.update_odometer(15)
 my_new_car.read_odometer()
