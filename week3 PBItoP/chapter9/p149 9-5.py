3fc0c64422dda4ee31e4661ba33fd57cbb71555a
lishubin <2697601945@qq.com>
2022-09-11 11:39:14 +0800

    p149 9-5练习完成

--- week3 PBItoP/chapter9/p149 9-5.py
+++ week3 PBItoP/chapter9/p149 9-5.py
@@ -3,11 +3,11 @@
 # @File : p149 9-5.py
 # @Software :PyCharm
 
-class user:
+class User:
     def __init__(self,first_name,last_name):
         self.first_name = first_name
         self.last_name = last_name
-        self.loginattempts = 0
+        self.login_attempts = 0
 
     def describe_user(self):
         full_name = f"{self.first_name} {self.last_name}"
@@ -15,11 +15,21 @@ class user:
     def greet_user(self):
         print(f"你好，{self.describe_user()}")
 
-    def login_attempts(self):
-        print(f"您的登陆次数是{self.loginattempts}")
+    def login_attempt(self):
+        print(f"您的登陆次数是{self.login_attempts}")
 
     def increment_login_attempts(self):
-        self.loginattempts += 1
+        self.login_attempts += 1
 
     def reset_login_attempts(self):
-        self.loginattempts = 0
+        self.login_attempts = 0
+
+
+
+user_1 = User("Bingbing","Chen")
+user_1.greet_user()
+user_1.increment_login_attempts()
+user_1.login_attempt()
+user_1.increment_login_attempts()
+
+user_1.login_attempt()
