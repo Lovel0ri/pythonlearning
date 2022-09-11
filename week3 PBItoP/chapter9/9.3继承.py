dc6170db9c5dab773c69914ebdba8733d1f0445a
lishubin <2697601945@qq.com>
2022-09-11 13:28:19 +0800

    将实例用做属性

--- week3 PBItoP/chapter9/9.3继承.py
+++ week3 PBItoP/chapter9/9.3继承.py
@@ -36,21 +36,39 @@ class Car:
     def fill_gas_tank(self):
         print(f"您的汽车还有{self.fill_gas} L油")
 
+class Battery:
+    """一次模拟电动车汽车电瓶的简单尝试"""
+
+    """初始化电瓶的属性"""
+    def __init__(self,battery_size = 75):
+        self.battery_size = 75
+
+    def describe_battery_size(self):
+        """打印一条描述电瓶容量的信息"""
+        print(f"这辆车拥有{self.battery_size}--kWh 的电瓶容量")
+
+    def get_range(self):
+        """打印一条消息，指出电瓶的续航里程"""
+        if self.battery_size == 75:
+            range = 250
+        elif self.battery_size == 100:
+            range = 310
+
+        print(f"您的车子的续航里程是{range}")
+
 class ElectricCar(Car):
     """电动汽车的独到之处"""
     def __init__(self,make,model,year):
         """初始化父类的属性"""
         super().__init__(make,model,year)
-        self.battery_size = 75
-
-    def describe_battery_size(self):
-        print(f"这辆车还剩下{self.battery_size}--kWh 的电量")
+        self.battery = Battery()
 
     def fill_gas_tank(self):
         print("您好，电动汽车没有油箱")
 
 my_tesla = ElectricCar("tesla","model s",2019)
 print(my_tesla.get_descriptive_name())
-my_tesla.describe_battery_size()
 
-my_tesla.fill_gas_tank()
+my_tesla.battery.describe_battery_size()
+"""这行代码让Python在实例my_tesla中查找属性battery，并对存储在该属性中的Battery实例调用方法describe_batter_size()"""
+my_tesla.battery.get_range()
