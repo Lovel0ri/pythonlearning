afd4d2cb494be0a8ec0c1da8464e7b42545f453a
lishubin <2697601945@qq.com>
2022-09-20 01:54:05 +0800

    unittest测试函数

---
+++ week5chapter11测试代码/test_name_function.py
@@ -0,0 +1,14 @@
+# @Time: 2022/9/20 1:39
+# @Author: 李树斌
+# @File : test_name_function.py
+# @Software :PyCharm
+import unittest
+from name_function import get_formatted_name
+class NamesTestCase(unittest.TestCase):
+
+     def test_first_last_name(self):
+         formatted_name = get_formatted_name('Bingbing','Chen')
+         self.assertEqual(formatted_name,'Bingbing Chen')
+
+if __name__ == '__main__':
+    unittest.main
